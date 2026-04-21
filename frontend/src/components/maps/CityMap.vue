0d449fec15f223f3032211729665c3b8b3ec5cdd
pastorello <luca@pastorello.info>
2026-04-21 20:44:13 +0200

    mounting map

---
+++ frontend/src/components/maps/CityMap.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="height: 400px; width: 100%">
+    <l-map ref="map" v-model:zoom="zoom" :center="[41.2172608, 13.5625165]">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        layer-type="base"
+        name="OpenStreetMap"
+      ></l-tile-layer>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import 'leaflet/dist/leaflet.css'
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet'
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      zoom: 14,
+    }
+  },
+}
+</script>
+
+<style></style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CityMap from '@/components/maps/CityMap.vue'
+import PanelBox from '@/components/boxes/PanelBox.vue'
+</script>
 
 <template>
-  <main>
-    <h1 class="text-3xl font-bold text-red-500 underline">Hello world!</h1>
-  </main>
+  <div class="max-w-7xl mx-auto grid grid-cols-6 gap-4">
+    <div class="col-span-2 main-left">
+      <PanelBox>
+        <h1 class="text-3xl font-bold">Punti di interesse</h1>
+      </PanelBox>
+    </div>
+
+    <div class="col-span-4 main-right">
+      <PanelBox>
+        <CityMap></CityMap>
+      </PanelBox>
+    </div>
+  </div>
 </template>
