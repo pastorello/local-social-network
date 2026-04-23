4b5cfa3ace33e5e52147c5c45bcb53367c611d19
pastorello <luca@pastorello.info>
2026-04-23 18:01:16 +0200

    install posts be app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,10 +8,9 @@ from account.views import activateemail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include([
-        path('', include('account.urls')),
-        path('', include('polls.urls')),
-    ])),
+    path('api/users/', include('account.urls')),
+    path('api/posts/', include('post.urls')),
+    path('api/polls/', include('polls.urls')),
     path('activateemail/', activateemail, name='activateemail'),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
